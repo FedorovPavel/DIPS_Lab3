fc6c589ebdcfd6c3204995096e9f41522682caec
Pavel <IU7.2@yandex.ru>
2017-11-21 20:10:32 +0300

    Добавление валидации и функций для работы с rabbitMQ

--- AggregationService/app/controllers/aggregation_api.js
+++ AggregationService/app/controllers/aggregation_api.js
@@ -1,11 +1,59 @@
 var express   = require('express'),
     router    = express.Router(),
     bus       = require('./../coordinator/bus'),
-    validator = require('./../validator/validator');
+    validator = require('./../validator/validator'),
+    amqp = require('amqplib/callback_api');
+    
 
 module.exports = function (app) {
   app.use('/aggregator', router);
 };
+
+function addIdOrderToQueue(id) {
+  amqp.connect('amqp://localhost', function(err, conn){
+    conn.createChannel(function(err, ch){
+      var queue = 'orders_id';
+
+      ch.assertQueue(queue, {durable : false});
+      ch.sendToQueue(queue, Buffer.from(id));
+      console.log('ID :' + id + 'added to queue [' + queue + ']');
+    });
+    setTimeout(function() {conn.close()},500);
+  });
+}
+
+function receiveIdOrderFromQueue(){
+  amqp.connect('amqp://localhost', function(err, conn){
+    conn.createChannel(function(err, ch){
+      var queue = 'orders_id';
+
+      ch.assertQueue(queue, {durable : false});
+      ch.consume(queue, function(id){
+        console.log('in queue exist id: ' + id);
+        return id;
+      }, {noAck : true});
+    });
+  });
+}
+
+setInterval(function(){
+  bus.checkOrderService(function(err, status){
+    if (status == 200){
+      const id = receiveIdOrderFromQueue();
+      bus.orderComplete(id, function(err, status){
+        if (err)
+          addIdOrderToQueue(id);
+        else {
+          if (status == 200){
+            console.log('id '+ id + 'processed');
+          } else {
+            addIdOrderToQueue(id);
+          }
+        }
+      });
+    }
+  });
+}, 20000);
 
 // Get any cars
 router.get('/catalog', function(req, res, next){
@@ -35,13 +83,33 @@ router.get('/catalog/:id', function(req, res, next){
   }
 });
 
-router.post('/orders/:id', function(req, res, next){
-  const param = {
-    userID  : req.params.id,
-    carID   : req.body.carID,
-    startDate : req.body.startDate,
-    endDate   : req.body.endDate
-  };
+//  Create Order
+router.post('/orders/', function(req, res, next){
+  const param = {};
+  const userID = validator.checkID(req.body.userID);
+  if (typeof(userID) == 'undefined'){
+    res.status(400).send({status : 'Error', message : 'Bad request : Invalid user ID'});
+    return;
+  }
+  param.userID = userID;
+  const carID = validator.checkID(req.body.carID);
+  if (typeof(carID) == 'undefined'){
+    res.status(400).send({status : 'Error', message : 'Bad request : Invalid car ID'});
+    return;
+  }
+  param.carID = carID;
+  const startDate = validator.ConvertStringToDate(req.body.startDate);
+  if (startDate){
+    res.status(400).send({status : 'Error', message : 'Bad request : Invalid start rent date'});
+    return;
+  }
+  param.startDate = startDate;
+  const endDate = validator.ConvertStringToDate(req.body.endDate);
+  if (!item.EndDate) {
+    res.status(400).send({status : 'Error', message : 'Bad request : Invalid end rent date'});
+    return;
+  }
+  param.endDate;
   bus.createOrder(param, function(err, status, response){
     if (err)
       return next(err);
@@ -51,8 +119,13 @@ router.post('/orders/:id', function(req, res, next){
   });
 });
 
-router.get('/orders/:id/getOrder/:order_id', function(req, res, next){
-  const order_id = req.params.order_id;
+//  Get order
+router.get('/orders/:order_id', function(req, res, next){
+  const order_id = validator.checkID(req.params.order_id);
+  if (typeof(order_id) == 'undefined') {
+    res.status(400).send({status : 'Error', message : 'Bad request: Invalid ID'});
+    return;
+  }
   bus.getOrder(order_id, function(err, status, response){
     if (err)
       return next(err);
@@ -62,20 +135,67 @@ router.get('/orders/:id/getOrder/:order_id', function(req, res, next){
   });
 });
 
-router.get('/orders/getOrders/:id/:page/:count', function(req, res, next){
-  const id = req.params.id;
-  const page = req.params.page;
-  const count = req.params.count;
-  bus.getOrders(id, page, count, function(err, status, response){
+//  Get orders
+router.get('/orders', function(req, res, next){
+  let page  = validator.checkPageNumber(req.query.page);
+  let count = validator.checkCountNumber(req.query.count);
+  bus.getOrders(page, count, function(err, status, orders){
     if (err)
-      return next(err);
+      res.status(status).send(orders);
     else {
-      res.status(status).send(response);
+      let carId = [];
+      let billingId = [];
+      for (let I = 0; I < orders.length; I++){
+        if (carId.indexOf(orders[I].CarID) == -1)
+          carId.push(orders[I].CarID);
+        if (typeof(orders[I].BillingID) != 'undefined')
+          billingId.push(orders[I].BillingID);
+      }
+      bus.getCarsByIDs(carId,function(err, status, cars){
+        if (err){
+          for (let I = 0; I < orders.length; I++)
+            orders[I].CarID = "Неизвестно";
+        } else {
+          cars = Array.from(cars);
+          for (let I = 0; I < orders.length; I++){
+            const car = orders[I].CarID;
+            delete orders[I].CarID;
+            const index = cars.findIndex(function(elem, index, arr){
+              if (elem.id == car)
+                return index;
+              return false;
+            });
+            orders[I].Car = cars[index];
+          }
+        }
+        busGetBillingsIDs(billingId, function(err, status, billings){
+          if (err) {
+            for (let I = 0; I < orders.length; I++){
+              orders[I].BillingID = 'Неизвестно';
+            }
+          } else {
+            billings = Array.from(billings);
+            for (let I = 0; I < orders.length; I++) {
+              const billing = orders[I].BillingID;
+              delete orders[I].BillingID;
+              if (typeof(billing) != 'undefined'){
+                const index = billings.findIndex(function(elem, index, arr){
+                  if (elem.id == billing)
+                    return index;
+                  return false;
+                });
+                orders[I].Billing = billings[index];
+              }
+            }
+          }
+          res.status(200).send(orders);
+        });
+      });
     }
   });
 });
 
-router.post('/orders/:id/order_paid', function(req, res, next){
+router.post('/orders/paid/:id', function(req, res, next){
   const id = req.params.id;
   const data = {
     paySystem : req.body.paySystem,
@@ -103,13 +223,18 @@ router.post('/orders/:uid/confirm_order/:oid', function(req, res, next){
   });
 });
 
-router.post('/orders/:uid/completed_order/:oid', function(req, res, next){
+router.post('/orders/completed/:oid', function(req, res, next){
   const uid = req.params.id;
   const id = req.params.oid;
   bus.orderComplete(id, function(err, status, response){
-    if (err)
-      return next(err);
-    else {
+    if (err){
+      if (status == 500) {
+        addIdOrderToQueue(id);
+        res.status(202).send('Change status succesfully');
+      }else {
+        res.status(status).send(response);
+      }
+    } else {
       res.status(status).send(response);
     }
   });
